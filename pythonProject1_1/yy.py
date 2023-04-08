721f430983c522e8968909f04fc73588af1fd43a
jenkinshe <760402621@qq.com>
2023-04-09 00:41:12 +0800

    提交一个yy.py文件

---
+++ pythonProject1_1/yy.py
@@ -0,0 +1,57 @@
+# head=[1,1,2,5,5,7]
+# new_list=[]
+#
+# for a in head:
+#     if a not in new_list:
+#         new_list.append(a)
+# print(new_list)
+
+# x=[1,5,6,2,8,5,4]
+# y=sorted(x)
+# print(y)
+
+# a=14
+# b=3
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(divmod(a,b))
+
+# a=int(input('请输入被除数：'))
+# b=int(input('请输入除数:'))
+# div=a//b
+# mod=a%b
+# print('取商，a//b等于：',div)
+# print('取余，a%b等于：',mod)
+
+# a= 'cxddsvdvv'
+# b='x'
+# ss=a.find(b)
+# print(ss)
+
+
+# a=[1,5,9,8,2,1]
+# print(a.index(5))
+# a.reverse()
+# print(a)
+# a.sort()
+# print(a)
+# b=sorted(a,reverse=True)
+# print(b)
+# a=11
+# b=1
+
+# c1=int(a,2)
+# c2=int(b,2)
+# c3=c1+c2
+# m=bin(c3).replace('0b','')
+n=2
+num_list=range(n)
+b=bin(1).replace('0b','')
+print(b)
+# for a in num_list:
+#     b=bin(a).replace('0b','')
+#     if b
+
+
+
