88fd3a6d59cccd86d26c566a41e177b1c07e4764
jenkinshe <760402621@qq.com>
2023-04-09 00:29:07 +0800

    给master提交一次runner

---
+++ runner.py
@@ -0,0 +1,8 @@
+# l = [2,1,2,3,4,5,6,6,5,4,3,2,1]
+# new_list = []
+# for i in l:
+#     if i not in new_list:
+#         new_list.append(i)
+# print(new_list)
+
+print(sum([i for i in range(1,101)]))
